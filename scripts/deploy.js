949fadafc8aa7cf543119fb76a607309c8307f14
Sonu Sagar <41724871+Sonu6957@users.noreply.github.com>
2022-05-20 16:38:07 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,22 +6,6 @@ async function main() {
 	"Deploying contracts with the account:",
 	deployer.address
 	);
-
-	console.log("Account balance:", (await deployer.getBalance()).toString());
-
-	const tokencontract = await ethers.getContractFactory("MyToken");
-	const tokendeployment = await tokencontract.deploy();
-    const ICOcontract = await ethers.getContractFactory("ICO");
-	const ICOdeployment = await ICOcontract.deploy(deployer.address,tokendeployment.address);
-    
-    const purchaseaddress="0xdD870fA1b7C4700F2BD7f44238821C26f7392148";
-    const deployed =await tokendeployment.transfer(ICOdeployment.address,await tokendeployment.totalSupply());
-    // const transferamount= await ICOdeployment.purchase(deployer.address,{ value: ethers.utils.parseUnits("74000000000000","wei") });
-    
-    console.log("ICOdepl ",deployed);
-    // console.log("Amount transferred is :",transferamount);
-	console.log("TokenContract deployed at:", tokendeployment.address);
-    console.log("ICOContract deployed at:", ICOdeployment.address);
 }
 
 main()
